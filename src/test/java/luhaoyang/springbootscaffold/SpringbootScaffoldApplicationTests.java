1bd44e58c1382061ab39c6bb1657ee7a7ee4ec20
yangluhao <yangluhao123@gmail.com>
2021-04-03 20:43:27 +1300

    initial proj

---
+++ src/test/java/luhaoyang/springbootscaffold/SpringbootScaffoldApplicationTests.java
@@ -0,0 +1,13 @@
+package luhaoyang.springbootscaffold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootScaffoldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
